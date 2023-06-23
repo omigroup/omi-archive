31fb1ffcfaec716d26055edd5c81248c97b342a2
madjin <32600939+madjin@users.noreply.github.com>
2023-06-22 20:12:47 -0400

    update pdfs

--- scripts/pdf.sh
+++ scripts/pdf.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+repo="$1"
+
+find $repo/archive -maxdepth 1 -type f -iname "*.jpg" -exec basename {} .jpg \; | sort -n > "$repo/numbers.txt"
+
 # Read numbers.txt line by line
 while read -r number; do
-  chromium --headless --disable-gpu --print-to-pdf=$number.pdf https://github.com/omigroup/gltf-extensions/discussions/$number
-done < numbers.txt
+  pdf_file="$repo/pdf/$number.pdf"
+  
+  # Check if the PDF file already exists
+  if [ ! -f "$pdf_file" ]; then
+    chromium --headless --disable-gpu --print-to-pdf="$pdf_file" "https://github.com/omigroup/$repo/discussions/$number"
+  fi
+done < "$repo/numbers.txt"
+
+# clean up files
+rm "$repo/numbers.txt"
